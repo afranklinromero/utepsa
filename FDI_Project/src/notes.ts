f5a6b92f6cf8676df6917befab0c15574c8b5dee
CmdrCloud <a.garoufalias@gmail.com>
2024-06-05 21:48:40 -0400

    Actualizando Proyecto

---
+++ FDI_Project/src/notes.ts
@@ -0,0 +1,13 @@
+/*
+1: User auth firebase
+2: Firebase
+3: CRUD for the db
+4: Anchors don't work
+5: Clean up index.html
+    5.1: Add border radius to the lady image 
+    5.2: Add a background colour to footer to indicate that the webpage has ended 
+    5.3: Figure out what to do with the contact form, maybe just insert a gif of the note-taking working
+    5.4: Change the dropdown menu from on click to on hover
+    5.5: Delete whatsapp and instagram svgs 
+    5.6: Add the notestream logo in the center of the footer and make it an anchor to go back to the top of the page/refresh it
+*/
